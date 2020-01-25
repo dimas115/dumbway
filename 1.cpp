3835a1c0cb22fbbd8bba33ea16c1bad5e6295a38
dimas115 <59861935+dimas115@users.noreply.github.com>
2020-01-25 21:39:30 +0700

    via file upload
    soal pertama menggunakan aplikasi dev c++ dengan bahasa pemrogaman c++

---
+++ 1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int m,n,r;
+    cout<<"masukan jumlah burung = ";
+    cin>>m;
+    cout<<"Jumlah lovebird dalam "<<441<<" Hari adalah "<<m*0.11+m*21 <<endl;
+    return 0;
+}
